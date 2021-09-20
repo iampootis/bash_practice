05f9786011466b70401e98c1d88f06fe34af4965
Egor Funtikov <gfalken@bk.ru>
2021-09-20 22:17:50 +0300

    Simple idea

--- add_user.sh
+++ add_user.sh
@@ -1,18 +1,34 @@
 #!/bin/bash
 
+
+if [ -z "$*" ]
+then
+echo "No options found!"
+exit 1
+fi
+
+while getopts "g:h:p:n:" opt; do
+case $opt in
+g) echo "User groups: $OPTARG"
+user_groups="$OPTARG"
+;;
+h) echo "User's home directory: $OPTARG"
+home_dir="$OPTARG"
+;;
+p) echo "Password entered"
+password="$OPTARG"
+;;
+n) echo "User name: $OPTARG"
+user_name="$OPTARG"
+;;
+*) echo "No reasonable options found!";;
+esac
+done
+
+
 function add_user() {
-  useradd -G ["group_1", "group_2"] -d $2 -p $(openssl passwd $3) $4  # Groups, home_dir, password's hash, user_name
+  useradd -G "$1" -d "$2" -p "$(openssl passwd password)" "$4"  # Groups, home_dir, password's hash, user_name
 }
 
-function split_users() {
-  IFS=";" read -a myarray <<< $1
-  echo "Groups: ${myarray[0]}"
-  echo "Password: ${myarray[1]}"
-  echo "Name: ${myarray[2]}"
-}
-
-function split_groups() {
-
-}
-
-split_users "$@"
+# printf "\n$user_groups\n$home_dir\n$password\n$user_name\n"
+add_user "$user_groups" "$home_dir" "$password" "$user_name"
